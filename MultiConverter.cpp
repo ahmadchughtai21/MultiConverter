dc02c29902fa04448ffad1a1a4a38e513187b858
Ikrash Bakhsh <miqrashbaksh@gmail.com>
2024-01-08 23:39:14 +0500

    Update MultiConverter.cpp

--- MultiConverter.cpp
+++ MultiConverter.cpp
@@ -13,6 +13,20 @@ int main()
     cout <<"=  ##### #    #    #    ####  #     #        #        #    #    #    ####   #     # =" << endl;
 	cout<<" =================================================================================="<<endl;
 	cout<<"\n\t\t\t\t      ++++MENU++++"<<endl;
+	
+	
+//menu
+
+	cout <<"Enter a Choice from the Options Below :" <<endl;
+	cout <<"1. Calculator" <<endl;
+	cout <<"1. Calculator" <<endl;
+	cout <<"1. Calculator" <<endl;
+	cout <<"1. Calculator" <<endl;
+	cout <<"1. Calculator" <<endl;
+	cout <<"1. Calculator" <<endl;
+	cout <<"1. Calculator" <<endl;
+	cout <<"1. Calculator" <<endl;
+	
     return 0;
 }
 
